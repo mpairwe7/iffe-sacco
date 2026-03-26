aeccf9568dc7d3fff4f3c48535f0da48079175fa
darkhorse <darkhorse@localhost.localdomain>
2026-03-26 09:17:26 +0300

    Use standard @prisma/client from node_modules instead of custom output

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neon } from "@neondatabase/serverless";
 
